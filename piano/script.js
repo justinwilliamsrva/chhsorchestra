4e5eee24a84a217b95d6b529d241076e3397fda9
justinwilliamsrva <justin.williams2@me.com>
2020-09-14 10:25:40 -0400

    scores

--- piano/script.js
+++ piano/script.js
@@ -8,8 +8,9 @@ window.onload = function () {
         rodrick.a1("c")
         brady.a1(4, "Correct")
         jacob.a1(2,"not the correct notes")
-        reuben.a2(3, "Rhyhtm is not correct for second warmup. You hold the second quarter notes too long")
+        reuben.a2(4, "Good")
         // joseph.a1(4," Good you don't have to repeat")
+      
 
 
     }
