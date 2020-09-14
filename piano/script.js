d76e73bd67238181dc061fe1ab92fde6ded94aa9
justinwilliamsrva <justin.williams2@me.com>
2020-09-14 10:24:17 -0400

    code

--- piano/script.js
+++ piano/script.js
@@ -9,7 +9,7 @@ window.onload = function () {
         brady.a1(4, "Correct")
         jacob.a1(2,"not the correct notes")
         reuben.a2(3, "Rhyhtm is not correct for second warmup. You hold the second quarter notes too long")
-        joseph.a1(4," Good you don't have to repeat")
+        // joseph.a1(4," Good you don't have to repeat")
 
 
     }
